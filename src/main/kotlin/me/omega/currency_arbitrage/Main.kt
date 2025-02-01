e7b30693da9f1ea6a6f761d2334e8131bfa4446f
omegaladon <businessomga@gmail.com>
2025-02-01 02:01:25 -0800

    Update Main.kt

--- src/main/kotlin/me/omega/currency_arbitrage/Main.kt
+++ src/main/kotlin/me/omega/currency_arbitrage/Main.kt
@@ -1,16 +1,79 @@
-package me.omega
+package me.omega.currency_arbitrage
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val currencies = Currency.entries.toTypedArray()
+    val exchangeRates = arrayOf(
+        /*                       CNY      EUR      JPY      NZD     USD  */
+        /* CNY */ doubleArrayOf(1.0000, 0.1406, 19.86490, 0.2318, 0.1503),
+        /* EUR */ doubleArrayOf(7.2861, 1.0000, 138.8341, 1.6753, 1.0797),
+        /* JPY */ doubleArrayOf(0.0527, 0.0071, 1.000000, 0.0120, 0.0076),
+        /* NZD */ doubleArrayOf(4.3228, 0.5962, 78.26350, 1.0000, 0.6773),
+        /* USD */ doubleArrayOf(7.0274, 0.9219, 111.5819, 1.5753, 1.0000)
+    )
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val data = CurrencyData(currencies, exchangeRates)
+
+    // Algorithm
+    println("Algorithm output:")
+    val cycle = detectArbitrage(data)
+    println("Cycle (${cycle.sumOf { it.negativeLog }}): ${cycle.joinToString(" -> ")}")
+    println("Profit: ${cycle.map { it.rate }.reduce { acc, d -> acc * d }}")
+}
+
+fun detectArbitrage(data: CurrencyData): List<Edge> {
+    val vertices = data.currencies.size
+    val edges = data.edges.size
+
+    val dist = DoubleArray(vertices) { Double.MAX_VALUE }
+    dist[0] = 0.0
+
+    val predecessors = IntArray(vertices) { -1 }
+
+    for (i in 0 until vertices - 1) {
+        for (j in 0 until edges) {
+            val edge = data.edges[j]
+            val from = edge.from.ordinal
+            val to = edge.to.ordinal
+            val weight = edge.negativeLog
+
+            if (dist[from] != Double.MAX_VALUE && dist[from] + weight < dist[to]) {
+                dist[to] = dist[from] + weight
+                predecessors[to] = from
+            }
+        }
     }
+
+    val possibleCycles = mutableListOf<List<Edge>>()
+
+    for (i in 0 until edges) {
+        val edge = data.edges[i]
+        var from = edge.from.ordinal
+        val to = edge.to.ordinal
+        val weight = edge.negativeLog
+
+        if (dist[from] != Double.MAX_VALUE && dist[from] + weight < dist[to]) {
+            // Negative cycle exists
+            predecessors[to] = from
+            val cycle = mutableListOf(to, from)
+            while (true) {
+                from = predecessors[from]
+                if (cycle.contains(from)) {
+                    break
+                }
+                cycle += from
+            }
+            cycle += to
+            cycle.reverse()
+
+            val cycleEdges = mutableListOf<Edge>()
+            for (j in 0 until cycle.size - 1) {
+                cycleEdges += data.getEdge(cycle[j], cycle[j + 1])
+            }
+            if (cycleEdges.size > 2) {
+                possibleCycles += cycleEdges
+            }
+        }
+    }
+
+    return possibleCycles.maxByOrNull { it.map { it.rate }.reduce { acc, d -> acc * d } } ?: emptyList()
 }
